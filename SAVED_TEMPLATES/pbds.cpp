168be0e58da681f37bec4460cd1810e0033122c9
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-12-15 21:02:29 +0530

    Update pbds.cpp

--- SAVED_TEMPLATES/pbds.cpp
+++ SAVED_TEMPLATES/pbds.cpp
@@ -3,7 +3,6 @@ Berozgaar hu, naukri chahye!
 */
 
 #include <bits/stdc++.h>
-
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
@@ -11,7 +10,6 @@ using namespace std;
 using namespace __gnu_pbds;
   
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-
 
 #define ll long long
 #define int            long long 
@@ -22,19 +20,13 @@ using namespace __gnu_pbds;
 
 #define yes cout<<"YES"<<endl
 #define no cout<<"NO"<<endl
-
 const int N = 2000005;
 int mod = 1000000007 ;
 
 
-
-
 int32_t main(){
-	//ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	#ifndef ONLINE_JUDGE
-    freopen("input.txt",  "r",  stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
  
 	int t = 1;
 	// cin>>t;
@@ -48,9 +40,9 @@ int32_t main(){
 			cin>>x;
 			st.insert(x);
 		}
-		int key;
-		cin>>key;
-		cout<<st.order_of_key(key);
+		int k;
+		cin>>k;
+		cout<<st.order_of_key(k)<<endl;
 
 	}
 
